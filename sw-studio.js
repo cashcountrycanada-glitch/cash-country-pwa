66dc61ea2edd436cbfb76261e6dff7df4cfd1f96
cashcountrycanada-glitch <cashcountrycanada@gmail.com>
2026-05-13 21:20:09 -0400

    Add files via upload

--- sw-studio.js
+++ sw-studio.js
@@ -6,7 +6,7 @@
  * qui les isole correctement en modules ES natifs.
  */
 
-const CACHE = 'studio-v41-html-fix'; // v36: fix \\U escape + double-brace useStudioOffline
+const CACHE = 'studio-v42-emoji-fix'; // v36: fix \\U escape + double-brace useStudioOffline
 
 const CRITICAL = [
   '/index-pwa.html',
