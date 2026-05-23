f4d822c09fe7167952c955139b9cacf3d244e413
cashcountrycanada-glitch <cashcountrycanada@gmail.com>
2026-05-23 00:58:34 -0400

    Add files via upload
    59

--- sw-studio.js
+++ sw-studio.js
@@ -19,7 +19,7 @@
  * ╚══════════════════════════════════════════════════════════════════╝
  */
 
-const CACHE = 'studio-v152';
+const CACHE = 'studio-v153';
 
 const CRITICAL = [
   '/index-pwa.html',
